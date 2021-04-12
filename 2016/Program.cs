808c9ef7bb6065f7d2d806ae6f90f92bf6ab6a7b
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2021-04-12 20:50:14 +0900

    코딩테스트 2016년

---
+++ 2016/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016
+{
+    public class Solution
+    {
+        public string solution(int a, int b)
+        {
+            DateTime date = new DateTime(2016, a, b);
+
+            //DayOfWeek => 해당 요일 가지고옴
+            //Substring(index, length) 0부터 lenght만큼 자른다
+            //ex) Tuesday 앞에 3개만.Substring(0,3) => Tue
+            return date.DayOfWeek.ToString().Substring(0, 3).ToUpper();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            string date = sol.solution(4, 5);
+            Console.WriteLine(date);
+        }
+    }
+}
