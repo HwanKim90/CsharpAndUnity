87ab23640f1c77c3b700f7f2aae13446558b12f5
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2021-04-14 20:05:01 +0900

    코딩테스트 가운데 글자 가져오기

---
+++ BringTheMiddleLetter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BringTheMiddleLetter
+{
+    class Program
+    {
+        public class Solution
+        {
+            public string solution(string s)
+            {
+                string answer = "";
+                int center = s.Length / 2;
+                if (s.Length % 2 == 0)
+                {
+                    answer = s[center - 1].ToString() + s[center].ToString(); 
+                }
+                else
+                {
+                   answer = s[center].ToString();
+                }
+                return answer;
+            }
+
+        }
+        /*
+        public string solution(string s)
+        {
+            int n = (s.Length + 1) % 2;
+            int l = s.Length / 2 - n;
+            return s.Substring(l, n + 1);
+        }
+        */
+        static void Main(string[] args)
+        {
+            string s = "abcde";
+            Solution sol = new Solution();
+
+            String result = sol.solution(s);
+            Console.WriteLine(result);
+        }
+    }
+}
+
+
+
+
+
+
