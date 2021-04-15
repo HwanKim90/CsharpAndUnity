db6caeee5dc88324f8ce76ab1b25dc4412cd1536
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2021-04-15 12:51:30 +0900

    코딩테스트 나누어 떨어지는 숫자 배열

---
+++ Division/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division
+{
+    class Program
+    {
+        public class Solution
+        {
+            public int[] solution(int[] arr, int divisor)
+            {
+                List<int> list = new List<int>();
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] % divisor == 0)
+                    {
+                        list.Add(arr[i]);
+                    }
+
+                }
+
+                if (list == null || !list.Any())
+                {
+                    list.Add(-1);
+                }
+
+                list.Sort();
+                return list.ToArray();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 9, 7, 10 };
+            int divisor = 5;
+
+            Solution sol = new Solution();
+            
+            int[] result = sol.solution(arr, divisor);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+                
+        }
+    }
+}
+                
+
+
+
+
