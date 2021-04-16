8e37193685f5b97b775eb3992b6af69ccca74866
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2021-04-16 22:00:49 +0900

    코딩테스트 내적

---
+++ DotProduct/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotProduct
+{
+    class Program
+    {
+        public class Solution
+        {
+            public int solution(int[] a, int[] b)
+            {
+                int result = 0;
+                int answer = 0;
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    result = a[i] * b[i];
+                    answer += result;
+                }
+                return answer;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { -1,0,1 };
+            int[] b = { 1,0,-1 };
+
+            
+
+            Console.WriteLine(answer);
+        }
+    }
+               
+               
+               
+}
