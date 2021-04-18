5d2f1d64b005f5089be19bf8d095bcfd7b0d0d8e
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2021-04-18 18:03:57 +0900

    코딩테스트 예산

---
+++ Budget/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Budget
+{
+    public class Solution
+    {
+        public int solution(int[] d, int budget)
+        {
+            int answer = 0;
+            // 제일 작은수부터 예산 지정해주는 방법
+            Array.Sort(d);
+
+            for (int i = 0; i < d.Length; i++)
+            {
+                if (d[i] <= budget)
+                {
+                    budget -= d[i];
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+
+            
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] d = { 2, 2, 3, 3 };
+            int budget = 10;
+
+            Solution sol = new Solution();
+            int result = sol.solution(d, budget);
+            Console.WriteLine(result);
+            
+        }
+    }
+}
