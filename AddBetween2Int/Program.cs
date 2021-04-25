702b2ac6210fd25f659d03e8a430c25804b6a564
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2021-04-25 23:01:18 +0900

    코딩테스트 두 정수 사이의 합

---
+++ AddBetween2Int/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace AddBetween2Int
+{
+    public class Solution
+    {
+        public long solution(int a, int b)
+        {
+            long answer = 0;
+
+            while (a != b)
+            {
+                answer += a;
+                a = (a > b) ? a - 1 : a + 1;
+            }
+          
+            return answer + b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            Solution sol = new Solution();
+            long result = sol.solution(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
