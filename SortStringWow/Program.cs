bdb9c7c533e346af4fc947aa3db6a9882b841e59
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2021-04-26 22:29:49 +0900

    코딩테스트 문자열 내마음대로 정렬하기

---
+++ SortStringWow/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortStringWow
+{
+    public class Solution
+    {
+        public string[] solution(string[] strings, int n)
+        {
+            string[] answer = new string[strings.Length];
+            
+            for (int i = 0; i < strings.Length; i++)
+            {
+                answer[i] = strings[i][n] + strings[i];
+            }
+            Array.Sort(answer);
+            for (int i = 0; i <strings.Length; i++)
+            {
+                answer[i] = answer[i].Substring(1);
+            }
+
+            return answer;
+        }
+
+            
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = { "sun", "bed", "car" };
+            int n = 1;
+
+            Solution sol = new Solution();
+            string[] result = sol.solution(strings, n);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+    }
+}
